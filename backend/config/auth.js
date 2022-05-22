8718e3ecfe98ece6d0c05d179713f1e5af3a0616
Joao Carlos Alves <suporte_carlos@live.com>
2022-05-22 10:04:15 -0300

    finalizando auth backend

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -2,23 +2,23 @@ const jwt = require('jsonwebtoken')
 const env = require('../.env')
 module.exports = (req, res, next) => {
 
-  if(req.method === 'OPTIONS') {
-  next()
+  if (req.method === 'OPTIONS') {
+    next()
   } else {
-  const token = req.body.token || req.query.token ||
-req.headers['authorization']
-  if(!token) {
-  return res.status(403).send({errors: ['No token provided.']})
-  }
-  jwt.verify(token, env.authSecret, function(err, decoded) {
-  if(err) {
-  return res.status(403).send({
-  errors: ['Failed to authenticate token.']
-  })
-  } else {
+    const token = req.body.token || req.query.token ||
+      req.headers['authorization']
+    if (!token) {
+      return res.status(403).send({ errors: ['No token provided.'] })
+    }
+    jwt.verify(token, env.authSecret, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({
+          errors: ['Failed to authenticate token.']
+        })
+      } else {
 
-  next()
-  }
-  })
+        next()
+      }
+    })
   }
 }
